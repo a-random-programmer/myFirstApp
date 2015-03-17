ce47ba0c7e3372c48444fe32593ac19162fc42b9
Robert Hancock <Rob@Roberts-MacBook-Pro.local>
2015-03-17 15:14:12 -0700

    Testing repo update, added a comment

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var redButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //BUTTONS GALORE!!!
+    
+    @IBAction func labelRed(sender: AnyObject) {
+        colorLabel.textColor = UIColor.redColor()
+        redButton.setTitle("RED", forState: UIControlState.Highlighted)
+    }
+    
+    @IBAction func labelOrange(sender: AnyObject) {
+         colorLabel.textColor = UIColor.orangeColor()
+    }
+    
+    @IBAction func labelYellow(sender: AnyObject) {
+         colorLabel.textColor = UIColor.yellowColor()
+    }
+    
+    @IBAction func labelGreen(sender: AnyObject) {
+         colorLabel.textColor = UIColor.greenColor()
+    }
+    
+    @IBAction func switchRed(sender: AnyObject) {
+        colorLabel.backgroundColor = UIColor.redColor()
+    }
+    
+    @IBAction func switchOrange(sender: AnyObject) {
+        colorLabel.backgroundColor = UIColor.orangeColor()
+    }
 
-
+    @IBAction func switchYellow(sender: AnyObject) {
+        colorLabel.backgroundColor = UIColor.yellowColor()
+    }
+    
+    @IBAction func switchGreen(sender: AnyObject) {
+        colorLabel.backgroundColor = UIColor.greenColor()
+    }
 }
 
+	
